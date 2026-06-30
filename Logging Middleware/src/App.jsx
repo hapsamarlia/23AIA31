5f5d88702978c4231515c4f2c495188b9d79aa2d
Hapsamarlia07 <147903011+hapsamarlia@users.noreply.github.com>
2026-06-30 12:45:44 +0530

    Update App.jsx

---
+++ Logging Middleware/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/header";
+import Button from "./components/button";
+import Response from "./components/response";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Button />
+      <Response />
+    </div>
+  );
+}
+
+export default App;
